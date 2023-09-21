7000f91cfd5777c1aaca6b6e793456651744f663
alhad-tm <alhadchikkutm@gmail.com>
2023-09-21 17:09:32 +0530

    framerscroll testing

--- src/components/ScrollableComponent/ScrollableComponent.jsx
+++ src/components/ScrollableComponent/ScrollableComponent.jsx
@@ -1,40 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion, useAnimation, useViewportScroll } from 'framer-motion';
 import css from "./ScrollableComponent.module.css"
-import { useEffect } from 'react';
 
-const ScrollableComponent = () => {
+function ScrollableComponent() {
+  const { scrollY, scrollYProgress } = useViewportScroll();
+  const controls = useAnimation();
+  const [scrollbarHeight, setScrollbarHeight] = useState(0);
 
-    const [scrollPercentage, setScrollPercentage] = useState(0);
+  // Calculate scrollableHeight based on content and container height
+  useEffect(() => {
+    const contentHeight = document.querySelector('.content').clientHeight;
+    const containerHeight = document.querySelector('.scrollable-container').clientHeight;
+    setScrollbarHeight((containerHeight / contentHeight) * containerHeight);
+  }, []);
 
-    const handleScroll = () => {
-      const scrollableContainer = document.querySelector('.scrollable-container');
-      if (scrollableContainer) {
-        const { scrollTop, scrollHeight, clientHeight } = scrollableContainer;
-        const newScrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
-        setScrollPercentage(newScrollPercentage);
-      }
-    };
-  
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    return (
-      <div className={css.scrollablecontainer}>
+  scrollY.onChange((latest) => {
+    controls.start({ y: latest * -scrollbarHeight });
+  });
+
+  const handleScrollbarDrag = (_, info) => {
+    const newScrollY = -info.point.y / scrollbarHeight;
+    scrollY.set(newScrollY);
+  };
+
+  return (
+    <div className={css.scrollablecontainer}>
+      <div className={css.content}>
         {/* Content that exceeds the container height */}
         {Array.from({ length: 20 }, (_, index) => (
           <div key={index} className="content-item">
             Content Item {index + 1}
           </div>
         ))}
-
-        <h1>hello</h1>
-        <div className={css.progressbar} style={{ height: `${scrollPercentage}%` }}></div>
-      </div>)
+      </div>
+      <motion.div
+        className="scrollbar"
+        style={{ height: `${scrollbarHeight}px` }}
+        drag="y"
+        dragConstraints={{ top: 0, bottom: scrollbarHeight }}
+        dragElastic={0}
+        onDrag={handleScrollbarDrag}
+        onDragEnd={(_, info) => handleScrollbarDrag(_, info)}
+        animate={controls}
+      ></motion.div>
+    </div>
+  );
 }
 
-export default ScrollableComponent
- 
+export default ScrollableComponent;
